f9a06259d34e964dac46588320f91b69620af178
Y-DONALD <donaldyoungoua@gmail.com>
2024-02-22 19:30:53 +0100

    add theme config

--- src/errors/500.jsx
+++ src/errors/500.jsx
@@ -1,8 +1,18 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 function ServerError() {
     return (
-        <div>500</div>
+        <div className="flex items-center justify-center h-screen bg-gray-100">
+            <div className="text-center">
+                <h1 className="text-6xl font-bold text-gray-800">500</h1>
+                <p className="text-xl font-semibold text-gray-600">Internal Server Error</p>
+                <p className="mt-4 text-gray-700">Oops! Le serveur a rencontré une erreur et n'a pas été capable complèter votre requête.</p>
+                <p className="mt-4" style={{ fontSize: '20px' }}>
+                    Please visit <Link className='text-slate-400' to="/">Home page</Link>.
+                </p>
+            </div>
+        </div>
     )
 }
 
