2fef3073b03552591edd9f17f0403bfbcee64aaa
Y-DONALD <donaldyoungoua@gmail.com>
2024-02-22 16:27:37 +0100

    add router for the project

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,25 @@
-import React from 'react'
+import React from "react"
+import Home from "./pages/Home"
+import {
+  createBrowserRouter,
+  RouterProvider
+} from "react-router-dom";
+import Login from "./pages/Login";
+import ServerError from "./errors/500";
+import NotFound from "./errors/404";
+
+const router = createBrowserRouter([
+  { path: "/", element: <Home /> },
+  { path: "/login", element: <Login /> },
+  { path: "/500", element: <ServerError /> },
+  { path: "*", element: <NotFound /> }
+])
 
 function App() {
   return (
-    <>
-      <h1 className="text-3xl font-bold underline">
-        Hello world!
-      </h1>
-    </>
+    <div className="App">
+      <RouterProvider router={router} />
+    </div>
   )
 }
 
