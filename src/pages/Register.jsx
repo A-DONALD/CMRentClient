c51f05e8bf16b83902ada4ece376d5402d959e43
Y-DONALD <donaldyoungoua@gmail.com>
2024-02-27 13:43:59 +0100

    fix the bug of theme section

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -21,8 +21,14 @@ function Register() {
     const [validMatch, setValidMatch] = useState(false);
     const [matchFocus, setMatchFocus] = useState(false);
 
+    const [agree, setAgree] = useState(false);
+
     const [errMsg, setErrMsg] = useState('');
     const [success, setSuccess] = useState(false);
+
+    const handleCheckboxChange = (event) => {
+        setAgree(event.target.checked);
+    };
 
     useEffect(() => {
         userRef.current.focus();
@@ -156,14 +162,22 @@ function Register() {
                             </div>
                             <div className="flex items-start">
                                 <div className="flex items-center h-5">
-                                    <input id="terms" aria-describedby="terms" type="checkbox" className="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-primary-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-primary-600 dark:ring-offset-gray-800" required="" />
+                                    <input
+                                        id="terms"
+                                        aria-describedby="terms"
+                                        type="checkbox"
+                                        required
+                                        checked={agree}
+                                        onChange={handleCheckboxChange}
+                                        className="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-primary-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-primary-600 dark:ring-offset-gray-800"
+                                    />
                                 </div>
                                 <div className="ml-3 text-sm">
                                     <label htmlFor="terms" className="font-light text-gray-500 dark:text-gray-300">I accept the <a className="font-medium text-primary-600 hover:underline dark:text-primary-500" href="#">Terms and Conditions</a></label>
                                 </div>
                                 <div></div>
                             </div>
-                            <button type="submit" disabled={!validMail || !validPwd || !validMatch ? true : false} className="text-white w-full bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800 bg-blue-400 dark:bg-blue-600 disabled:bg-blue-100 dark:disabled:bg-blue-300">Create account</button>
+                            <button type="submit" disabled={!validMail || !validPwd || !validMatch || !agree ? true : false} className="text-white w-full bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800 bg-blue-400 dark:bg-blue-600 disabled:bg-blue-100 dark:disabled:bg-blue-300">Create account</button>
                             <p className="text-sm font-light text-gray-500 dark:text-gray-400">
                                 Already have an account? <a href="/login" className="font-medium text-primary-600 hover:underline dark:text-primary-500">Login here</a>
                             </p>
